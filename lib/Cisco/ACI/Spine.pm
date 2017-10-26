2c566263a6106ae2ae1a56dbb6013af4ab449820
Luke Poskitt <luke.poskitt@gmail.com>
2017-10-26 15:36:15 +1100

    New class Cisco::ACI::Spine

---
+++ lib/Cisco/ACI/Spine.pm
@@ -0,0 +1,28 @@
+package Cisco::ACI::Spine;
+
+use Moose;
+
+extends 'Cisco::ACI::FabricNode';
+
+1;
+
+__END__
+"adSt": "on",
+"childAction": "",
+"delayedHeartbeat": "no",
+"dn": "topology/pod-1/node-201",
+"extMngdBy": "",
+"fabricSt": "active",
+"id": "201",
+"lcOwn": "local",
+"modTs": "2017-09-06T04:31:09.799+11:00",
+"model": "N9K-C9336PQ",
+"monPolDn": "uni/fabric/monfab-default",
+"name": "aci-spine-fs1",
+"nameAlias": "",
+"role": "spine",
+"serial": "FDO21141NQY",
+"status": "",
+"uid": "0",
+"vendor": "Cisco Systems, Inc",
+"version": ""
