9ef02ba57bcbbb61d9def4de76c98c823823e7ee
Luke Poskitt <luke.poskitt@gmail.com>
2017-10-27 15:24:36 +1100

    New class Cisco::ACI::Eqptcapacity::McastUsage

---
+++ lib/Cisco/ACI/Eqptcapacity/McastUsage.pm
@@ -0,0 +1,43 @@
+package Cisco::ACI::Eqptcapacity::McastUsage;
+
+use Moose;
+
+has 'cnt'		=> (is => 'rw', isa => 'Int');
+has 'lastCollOffset'	=> (is => 'rw', isa => 'Int');
+has 'localEpAvg'	=> (is => 'rw', isa => 'Int');
+has 'localEpBase'	=> (is => 'rw', isa => 'Int');
+has 'localEpCapAvg'	=> (is => 'rw', isa => 'Int');
+has 'localEpCapBase'	=> (is => 'rw', isa => 'Int');
+has 'localEpCapCum'	=> (is => 'rw', isa => 'Int');
+has 'localEpCapLast'	=> (is => 'rw', isa => 'Int');
+has 'localEpCapMax'	=> (is => 'rw', isa => 'Int');
+has 'localEpCapMin'	=> (is => 'rw', isa => 'Int');
+has 'localEpCapPer'	=> (is => 'rw', isa => 'Int');
+has 'localEpCapRate'	=> (is => 'rw', isa => 'Num');
+has 'localEpCapSpct'	=> (is => 'rw', isa => 'Int');
+has 'localEpCapThr'	=> (is => 'rw');
+has 'localEpCapTr'	=> (is => 'rw', isa => 'Int');
+has 'localEpCapTrBase'	=> (is => 'rw', isa => 'Int');
+has 'localEpCum'	=> (is => 'rw', isa => 'Int');
+has 'localEpLast'	=> (is => 'rw', isa => 'Int');
+has 'localEpMax'	=> (is => 'rw', isa => 'Int');
+has 'localEpMin'	=> (is => 'rw', isa => 'Int');
+has 'localEpPer'	=> (is => 'rw', isa => 'Int');
+has 'localEpRate'	=> (is => 'rw', isa => 'Num');
+has 'localEpSpct'	=> (is => 'rw', isa => 'Int');
+has 'localEpThr'	=> (is => 'rw');
+has 'localEpTr'		=> (is => 'rw', isa => 'Int');
+has 'localEpTrBase'	=> (is => 'rw', isa => 'Int');
+has 'modTs'		=> (is => 'rw', isa => 'Str');
+has 'normalizedAvg'	=> (is => 'rw', isa => 'Int');
+has 'normalizedLast'	=> (is => 'rw', isa => 'Int');
+has 'normalizedMax'	=> (is => 'rw', isa => 'Int');
+has 'normalizedMin'	=> (is => 'rw', isa => 'Int');
+has 'normalizedSpct'	=> (is => 'rw', isa => 'Int');
+has 'normalizedThr'	=> (is => 'rw');
+has 'normalizedTr'	=> (is => 'rw', isa => 'Int');
+has 'normalizedTrBase'	=> (is => 'rw', isa => 'Int');
+has 'normalizedTtl'	=> (is => 'rw', isa => 'Int');
+has 'status'		=> (is => 'rw', isa => 'Str');
+
+1;
