b7375b366679728b6128cfddd72888d5b2a9b41a
Luke Poskitt <luke.poskitt@gmail.com>
2017-10-31 10:36:11 +1100

    Move Cisco::ACI::FvcapRule attributes from inherited to defined class (see ref.)

--- lib/Cisco/ACI/FvcapRule.pm
+++ lib/Cisco/ACI/FvcapRule.pm
@@ -1,11 +1,15 @@
 package Cisco::ACI::FvcapRule;
 
 use Moose;
+use Moose::Util::TypeConstraints;
+use namespace::autoclean;
 
 extends 'Cisco::ACI::Rule';
 
 has 'faultState'	=> (is => 'rw', isa => 'Str');
+has 'scope'             => (is => 'rw', isa => enum([qw[node policy-domain fabric]]));
 has 'subj'		=> (is => 'rw', isa => 'Str');
+has 'type'              => (is => 'rw', isa => 'Str');
 
 1;
 
--- lib/Cisco/ACI/Rule.pm
+++ lib/Cisco/ACI/Rule.pm
@@ -1,13 +1,9 @@
 package Cisco::ACI::Rule;
 
 use Moose;
-use Moose::Util::TypeConstraints;
-use namespace::autoclean;
 
 has 'constraint'	=> (is => 'rw', isa => 'Int');
-has 'desc'		=> (is => 'rw', isa => 'Str');
-has 'type'		=> (is => 'rw', isa => 'Str');
-has 'scope'		=> (is => 'rw', isa => enum([qw[node policy-domain fabric]]));
+has 'descr'		=> (is => 'rw', isa => 'Str');
 
 1;
 
