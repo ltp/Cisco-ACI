13f55136b826b609472b3619b99b4a503c3f9f37
Luke Poskitt <luke.poskitt@gmail.com>
2017-11-06 16:27:01 +1100

    Added fault_counts() and health() methods.

--- lib/Cisco/ACI/Eqpt/ExtCh.pm
+++ lib/Cisco/ACI/Eqpt/ExtCh.pm
@@ -5,6 +5,7 @@ use Moose;
 extends 'Cisco::ACI::Eqpt::Cont';
 
 has 'controlFPortSDPTimeout'	=> ( is => 'rw', isa => 'Str' );
+has 'dn'			=> ( is => 'rw', isa => 'Str' );
 has 'extChSt'			=> ( is => 'rw', isa => 'Str' );
 has 'macAddr'			=> ( is => 'rw', isa => 'Str' );
 has 'mfgTm'			=> ( is => 'rw', isa => 'Str' );
@@ -17,6 +18,35 @@ has 'rev'			=> ( is => 'rw', isa => 'Str' );
 has 'ser'			=> ( is => 'rw', isa => 'Str' );
 has 'vendor'			=> ( is => 'rw', isa => 'Str' );
 has '__aci'			=> ( is => 'rw' );
+
+sub health {
+        my $self = shift;
+
+        return Cisco::ACI::Health::Inst->new( 
+                $self->__aci->__jp->decode(
+                        $self->__aci->__request(
+                                $self->__aci->__get_uri( 
+                                        '/api/mo/'. $self->dn .'/health.json'
+                                )   
+                        )->content
+                )->{ imdata }->[0]->{ healthInst }->{ attributes }
+        )   
+}
+
+
+sub fault_counts {
+        my $self = shift;
+
+        return Cisco::ACI::FaultCounts->new( 
+                $self->__aci->__jp->decode(
+                        $self->__aci->__request(
+                                $self->__aci->__get_uri( 
+                                        '/api/mo/'. $self->dn .'/fltCnts.json'
+                                )   
+                        )->content
+                )->{ imdata }->[0]->{ faultCounts }->{ attributes }
+        )
+}
 
 1;
 
