6ed5f62a4591eb263b7f645b0c0a4d9517f37fc5
Luke Poskitt <luke.poskitt@gmail.com>
2018-10-18 15:57:50 +1100

    Stub subnets() method.

--- lib/Cisco/ACI/FvBD.pm
+++ lib/Cisco/ACI/FvBD.pm
@@ -8,6 +8,13 @@ has 'name'	=> ( is => 'rw', isa => 'Str' );
 has 'type'	=> ( is => 'rw', isa => 'Str' );
 has '__aci'	=> ( is => 'rw' );
 
+sub subnets {
+	my $self = shift;
+
+	#/api/node/mo/uni/tn-Deakin-Production/BD-VLAN-822-BD.xml?query-target=children
+	# target-subtree-class=fvSubnet
+}
+
 1;
 
 __END__
