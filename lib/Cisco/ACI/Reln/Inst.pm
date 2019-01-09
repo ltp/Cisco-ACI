2a38997c0c5395b380b7071c5d9ee8c7960fbd25
Luke Poskitt <luke.poskitt@gmail.com>
2019-01-09 16:05:47 +1100

    Correct attribute syntax.

--- lib/Cisco/ACI/Reln/Inst.pm
+++ lib/Cisco/ACI/Reln/Inst.pm
@@ -2,8 +2,8 @@ package Cisco::ACI::Reln::Inst;
 
 use Moose;
 
-has 'tCl' ( is => 'rw', isa => 'Str' );
-has 'tDn' ( is => 'rw', isa => 'Str' );
+has 'tCl'	=> ( is => 'rw', isa => 'Str' );
+has 'tDn'	=> ( is => 'rw', isa => 'Str' );
 
 1;
 
