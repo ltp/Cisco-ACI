6bd2b4ae9d5732a21fd26024b318a3ba18c4adb9
Luke Poskitt <luke.poskitt@gmail.com>
2019-11-08 14:21:20 +1100

    Added utility classes.

---
+++ lib/Cisco/ACI/Stats/Curr/L2IngrPkts.pm
@@ -0,0 +1,24 @@
+package Cisco::ACI::Stats::Curr::L2IngrPkts;
+
+use Moose;
+
+extends 'Cisco::ACI::Stats::Curr';
+
+has 'multicastCum'	=> (is => 'rw', isa => 'Int');
+has 'multicastPer'	=> (is => 'rw', isa => 'Int');
+has 'multicastRate'	=> (is => 'rw', isa => 'Int');
+has 'multicastSpct'	=> (is => 'rw', isa => 'Int');
+has 'multicastThr'	=> (is => 'rw', isa => 'Int');
+has 'multicastTr'	=> (is => 'rw', isa => 'Int');
+has 'multicastTrBase'	=> (is => 'rw', isa => 'Int');
+has 'repIntvEnd'	=> (is => 'rw', isa => 'Int');
+has 'repIntvStart'	=> (is => 'rw', isa => 'Int');
+has 'unicastCum'	=> (is => 'rw', isa => 'Int');
+has 'unicastPer'	=> (is => 'rw', isa => 'Int');
+has 'unicastRate'	=> (is => 'rw', isa => 'Int');
+has 'unicastSpct'	=> (is => 'rw', isa => 'Int');
+has 'unicastThr'	=> (is => 'rw', isa => 'Int');
+has 'unicastTr'		=> (is => 'rw', isa => 'Int');
+has 'unicastTrBase'	=> (is => 'rw', isa => 'Int');
+
+1;
---
+++ lib/Cisco/ACI/Stats/Curr/L2IngrPktsAg15min.pm
@@ -0,0 +1,7 @@
+package Cisco::ACI::Stats::Curr::L2IngrPktsAg15min;
+
+use Moose;
+
+extends 'Cisco::ACI::Stats::Curr::L2IngrPktsAg';
+
+1;
