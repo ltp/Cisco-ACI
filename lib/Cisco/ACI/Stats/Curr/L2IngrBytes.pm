fe3497c066b30d153e425182cf1144d1749bb31a
Luke Poskitt <luke.poskitt@gmail.com>
2019-11-08 13:41:11 +1100

    Added utility classes.

---
+++ lib/Cisco/ACI/Stats/Curr/L2IngrBytes.pm
@@ -0,0 +1,33 @@
+package Cisco::ACI::Stats::Curr::L2IngrBytes;
+
+use Moose;
+
+extends 'Cisco::ACI::Stats::Curr';
+
+has 'index'		=> (is => 'rw', isa => 'Int');
+has 'multicastAvg'	=> (is => 'rw', isa => 'Int');
+has 'multicastBase'	=> (is => 'rw', isa => 'Int');
+has 'multicastCum'	=> (is => 'rw', isa => 'Int');
+has 'multicastLast'	=> (is => 'rw', isa => 'Int');
+has 'multicastMax'	=> (is => 'rw', isa => 'Int');
+has 'multicastMin'	=> (is => 'rw', isa => 'Int');
+has 'multicastPer'	=> (is => 'rw', isa => 'Int');
+has 'multicastRate'	=> (is => 'rw', isa => 'Int');
+has 'multicastSpct'	=> (is => 'rw', isa => 'Int');
+has 'multicastThr'	=> (is => 'rw', isa => 'Int');
+has 'multicastTr'	=> (is => 'rw', isa => 'Int');
+has 'multicastTrBase'	=> (is => 'rw', isa => 'Int');
+has 'unicastAvg'	=> (is => 'rw', isa => 'Int');
+has 'unicastBase'	=> (is => 'rw', isa => 'Int');
+has 'unicastCum'	=> (is => 'rw', isa => 'Int');
+has 'unicastLast'	=> (is => 'rw', isa => 'Int');
+has 'unicastMax'	=> (is => 'rw', isa => 'Int');
+has 'unicastMin'	=> (is => 'rw', isa => 'Int');
+has 'unicastPer'	=> (is => 'rw', isa => 'Int');
+has 'unicastRate'	=> (is => 'rw', isa => 'Int');
+has 'unicastSpct'	=> (is => 'rw', isa => 'Int');
+has 'unicastThr'	=> (is => 'rw', isa => 'Int');
+has 'unicastTr'		=> (is => 'rw', isa => 'Int');
+has 'unicastTrBase'	=> (is => 'rw', isa => 'Int');
+
+1;
---
+++ lib/Cisco/ACI/Stats/Curr/L2IngrBytesAg15min.pm
@@ -0,0 +1,7 @@
+package Cisco::ACI::Stats::Curr::L2IngrBytesAg15min;
+
+use Moose;
+
+extends 'Cisco::ACI::Stats::Curr::L2IngrBytesAg';
+
+1;
